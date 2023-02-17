1c95b98e79f477d40617ed9f43a05e2e655a771e
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-17 10:32:17 -0500

    added async getPosts function

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import Link from 'next/link'
+
+async function getPosts() {
+  const res = await fetch('/api/getPosts')
+}
+
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className="py-4 px-48">
+      <Link
+        className="bg-teal-700 text-black
+       font-medium py-2 px-4 rounded-md
+       "
+        href="/dashboard"
+      >
+        Go to the dashboard
+      </Link>
     </main>
   )
 }
